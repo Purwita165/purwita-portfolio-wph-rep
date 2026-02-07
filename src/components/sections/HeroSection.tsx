de0c4ed9a91595f6875850d3d2e381b14f4dcfaf
Purwita Musaffa <purwita165@gmail.com>
2026-02-07 20:27:07 +0700

    feat: hero content structure rendered correctly

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,7 +1,15 @@
 export default function HeroSection() {
   return (
     <section style={{ padding: 40 }}>
-      <h1>HERO OK</h1>
+      <p>Alex’s Portfolio</p>
+      <h1>
+        I am a Front-End Developer <br />
+        & Web Programming Instructor
+      </h1>
+      <p>
+        Hi, I’m Alex, a passionate web developer with over 3 years of experience.
+      </p>
+      <button>View Portfolio</button>
     </section>
   );
 }
